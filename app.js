391e6f64e882b46979df325af846d0d6cc974904
Jim <grand.jimmy1@gamil.com>
2013-11-01 13:38:05 -0700

    Add the config for heroku

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+var config = require('./config/config.js');
+var express = require('express');
+var app = express();
+var http = require('http');
+var socketIo = require('socket.io');
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
+// Create the Http server
+var server = http.createServer(app);
+
+var developmentEnv = 'development' == app.get('env');
+
+// Configure the app
+app.configure(function () {
+  app.use(express.static(__dirname + '/public'));
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
 });
 
-io.sockets.on('connection', function(socket) {
-	socket.on('createHamster', function(data) {
-		socket.broadcast.emit('onHamsterCreated', data);
-	});
+if (developmentEnv) {
+  console.log('Development environment');
+  app.configure(function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+} else {
+  console.log('Production environment');
+  app.configure(function () {
+    app.use(express.errorHandler());
+  });
+}
 
-	socket.on('updateHamster', function(data) {
-		socket.broadcast.emit('onHamsterUpdated', data);
-	});
+// Listen io
+var io = socketIo.listen(server, { log: developmentEnv });
 
-	socket.on('moveHamster', function(data){
-		socket.broadcast.emit('onHamsterMoved', data);
-	});
+// The socket io methods.
+io.sockets.on('connection', function (socket) {
+  socket.on('createHamster', function (data) {
+    socket.broadcast.emit('onHamsterCreated', data);
+  });
 
-	socket.on('deleteHamster', function(data){
-		socket.broadcast.emit('onHamsterDeleted', data);
-	});
+  socket.on('updateHamster', function (data) {
+    socket.broadcast.emit('onHamsterUpdated', data);
+  });
+
+  socket.on('moveHamster', function (data) {
+    socket.broadcast.emit('onHamsterMoved', data);
+  });
+
+  socket.on('deleteHamster', function (data) {
+    socket.broadcast.emit('onHamsterDeleted', data);
+  });
 });
 
-// For Heroku port
-server.listen(process.env.PORT || 3000);
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ config/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jimmy.grand on 10/28/13.
+ */
+
+//process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
